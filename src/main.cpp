23297437cf2a858a5a08b46e3237641d01970dee
Joseph Daniel Moreno <joejetmoreno@gmail.com>
2025-05-06 16:19:46 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// Heavily referenced: https://youtube.com/playlist?list=PLhfAbcv9cehhkG7ZQK0nfIGJC_C-wSLrx&si=lpjEMfmYyjf_TN1X
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+    return(0);
+};
