5d015f9b0d3685398a30c8dad7f3d99c3468d174
Joseph Daniel Moreno <joejetmoreno@gmail.com>
2025-05-19 18:04:02 -0400

    In the middle of porting

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,6 @@
 #include "../include/Game.hpp"
 
 int main(int argc, char** argv) {
-    const int fps = 60;
-    const int frame_delay = 1000 / fps; // 1000 ms / fps = ms per frame
-
-    Uint32 frame_start;
-    int frame_time;
-
     Game game;
 
     game.init("Game0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 960, false);
@@ -29,4 +23,46 @@ int main(int argc, char** argv) {
     game.cleanUp();
 
     return(0);
+};
+
+static void mainLoop(void) {
+    const int fps = 60;
+    const int frame_delay = 1000 / fps; // 1000 ms / fps = ms per frame
+
+    Uint32 frame_start = SDL_GetTicks();;
+    int frame_time;
+
+    if (quit) {
+        SdlObj0.close();
+        #ifdef __EMSCRIPTEN__
+        emscripten_cancel_main_loop();
+        #else
+        exit(0);
+        #endif
+    }
+
+	// Emulate a cycle of the Chip-8 architecture.
+	Chip8.emulateCycle();
+
+	// Render and update the screen if Chip8.dispFlag == 0x01.
+	if (Chip8.dispFlag == 0x01)
+		SdlObj0.drawScreen(Chip8);
+
+    
+
+	frame_time = SDL_GetTicks() - frame_start;
+
+	if (frame_delay > frame_time)
+		SDL_Delay(frame_delay - frame_time);
+};
+
+extern "C" {
+	void normalQuit() {
+		SdlObj0.close();
+        #ifdef __EMSCRIPTEN__
+        emscripten_cancel_main_loop();
+        #else
+        exit(0);
+        #endif
+	}
 };
